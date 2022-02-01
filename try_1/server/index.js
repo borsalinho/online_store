de80bae859e65a8a0ff0c0a0b1760fb43682ff93
borsalinho <dfvgbhcvbn0@gmail.com>
2022-02-01 12:25:00 +0900

    все хуйня, давай по новой

--- try_1/server/index.js
+++ try_1/server/index.js
@@ -1,36 +1,7 @@
-require('dotenv').config()
 const express = require('express')
-const sequelize = require('./db')
-const models = require('./models/models')
-const cors = require('cors')
-const fileUpload = require('express-fileupload')
-const router = require('./routers/index')
-const errorHandler = require('./middleware/ErrorHandingMIddleware')
-const path = require ('path')
 
-const PORT = process.env.PORT || 5000
+const PORT = 5000
 
-const app=express()
-app.use(cors())
-app.use(express.json())
-app.use(express.static(path.resolve(__dirname,'static')))
-app.use(fileUpload({}))
+const app = express()
 
-app.use('/api', router)
-
-//ОБработка ошибок, последний Middleware
-app.use(errorHandler)
-
-
-const start = async () => {
-    try {
-        await sequelize.authenticate()
-        await sequelize.sync()
-        app.listen(PORT,() => console.log('Server started on port '+ PORT))
-    } catch (e) {
-        console.log(e)
-    }
-}
-
-start()
-
+app.listen(PORT, ()=> console.log('Server started on port '+ PORT))
--- try_1/server/routers/index.js
+++
@@ -1,7 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const productRouter = require ('./productRouter')
-
-router.use('/product',productRouter)
-
-module.exports = router
