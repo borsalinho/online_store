d669262c97d223036187546aa3492adde71bf4be
borsalinho <dfvgbhcvbn0@gmail.com>
2022-02-01 12:53:00 +0900

    настройка бд

--- try_1/server/index.js
+++ try_1/server/index.js
@@ -1,7 +1,18 @@
+require('dotenv').config()
 const express = require('express')
-
-const PORT = 5000
+const sequalize = require('./db')
+const PORT = process.env.PORT || 5000
 
 const app = express()
 
-app.listen(PORT, ()=> console.log('Server started on port '+ PORT))
+const start = async () => {
+    try{
+        await sequalize.authenticate()
+        await sequalize.sync()
+        app.listen(PORT, ()=> console.log('Server started on port '+ PORT))
+    } catch(e) {
+        console.log(e)
+    }
+}
+
+start()
