3cc3479fe49bfc43751e7da389d924f752340c8a
borsalinho <dfvgbhcvbn0@gmail.com>
2022-02-01 13:03:55 +0900

    добавил модель продукта

---
+++ try_1/server/models/models.js
@@ -0,0 +1,8 @@
+const sequalize = require('../db')
+const {DataTypes} = require('sequelize')
+
+const Product = sequalize.define('product', {
+    id:{type:DataTypes.INTEGER, primaryKey:true, autoIncrement:true},
+    price: {type:DataTypes>INTEGER},
+    size: {type:DataTypes>INTEGER},
+})
