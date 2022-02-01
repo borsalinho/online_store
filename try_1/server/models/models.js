bcdf0578cea69eb66195a48588c746ed9d011c0b
borsalinho <dfvgbhcvbn0@gmail.com>
2022-02-01 13:30:48 +0900

    появились таблица Product в pgADMIN

--- try_1/server/models/models.js
+++ try_1/server/models/models.js
@@ -3,6 +3,11 @@ const {DataTypes} = require('sequelize')
 
 const Product = sequalize.define('product', {
     id:{type:DataTypes.INTEGER, primaryKey:true, autoIncrement:true},
-    price: {type:DataTypes>INTEGER},
-    size: {type:DataTypes>INTEGER},
+    price: {type:DataTypes.INTEGER},
+    size: {type:DataTypes.INTEGER},
 })
+
+
+module.exports = {
+    Product
+}
